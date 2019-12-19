027dbfb59b1125d7e2a159e9cba25be1311b316b
longway777 <1376024@qq.com>
2019-12-20 03:42:48 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# PagerGallery
+利用ViewPager2 实现滑动图片浏览器
+
---
+++ app/src/main/java/com/example/pagergallery/PagerPhotoFragment.kt
@@ -0,0 +1,42 @@
+package com.example.pagergallery
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.viewpager2.widget.ViewPager2
+import kotlinx.android.synthetic.main.fragment_pager_photo.*
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class PagerPhotoFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_pager_photo, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        val photoList = arguments?.getParcelableArrayList<PhotoItem>("PHOTO_LIST")
+        PagerPhotoListAdapter().apply {
+            viewPager2.adapter = this
+            submitList(photoList)
+        }
+
+        viewPager2.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback(){
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                photoTag.text = "${position + 1}/${photoList?.size}"
+            }
+        })
+
+        viewPager2.setCurrentItem(arguments?.getInt("PHOTO_POSITION")?:0,false)
+    }
+}
